e080b041cc2031475dabb7ce8a809d6553523c0b
Robert Cotey <coteyr@coteyr.net>
2010-04-17 14:51:47 -0400

    fleximage integration

--- init.rb
+++ init.rb
@@ -3,4 +3,4 @@ require File.dirname(__FILE__) + '/lib/coteyr_pack.rb'
 require File.dirname(__FILE__) + '/lib/restful.rb'
 require File.dirname(__FILE__) + '/lib/semantic_form_builder.rb'
 require File.dirname(__FILE__) + '/lib/semantic_form_helper.rb'
-
+require File.expand_path(File.join(File.dirname(__FILE__), %w(lib fleximage)))
