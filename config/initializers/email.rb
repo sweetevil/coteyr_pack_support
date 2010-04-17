3603bd2d01243268cfbbb022128b4f1bab2ca7a8
Robert Cotey <coteyr@coteyr.net>
2010-04-17 16:42:19 -0400

    email config

---
+++ config/initializers/email.rb
@@ -0,0 +1,8 @@
+config.action_mailer.smtp_settings = {
+  :address        => 'smtp.gmail.com',
+  :port           => 587,
+  :domain         => 'somedomain.com',
+  :authentication => :login,
+  :user_name      => 'my_name@somedomain.com',
+  :password       => 'secret'
+}
