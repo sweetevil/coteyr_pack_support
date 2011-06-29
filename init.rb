5ada34042d94c977b7c052bb70763e6a225ba767
Robert Cotey <coteyr@coteyr.net>
2011-06-29 12:30:50 -0400

    include restful authentication in my list of plugins that I now have to psuydo mantain

--- init.rb
+++ init.rb
@@ -19,6 +19,9 @@ require File.dirname(__FILE__) + '/lib/coteyr_pack.rb'
 require File.dirname(__FILE__) + '/lib/restful.rb'
 require File.dirname(__FILE__) + '/lib/semantic_form_builder.rb'
 require File.dirname(__FILE__) + '/lib/semantic_form_helper.rb'
+require File.join(File.dirname(__FILE__), "lib", 'restful-authentication', 'lib', "authentication")
+require File.join(File.dirname(__FILE__), "lib", 'restful-authentication', 'lib', "authentication", "by_password")
+require File.join(File.dirname(__FILE__), "lib", 'restful-authentication', 'lib', "authentication", "by_cookie_token")
 require File.expand_path(File.join(File.dirname(__FILE__), %w(lib fleximage)))
 
 require 'action_mailer'
