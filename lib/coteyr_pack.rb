39946aceb7fff6803b5986a5d3a16d569613c273
Robert Cotey <coteyr@coteyr.net>
2012-03-07 13:09:00 -0500

    stuffs
    s

--- lib/coteyr_pack.rb
+++ lib/coteyr_pack.rb
@@ -22,7 +22,7 @@ require File.dirname(__FILE__) + '/semantic_form_builder.rb'
 require File.join(File.dirname(__FILE__), 'restful-authentication', 'lib', "authentication")
 require File.join(File.dirname(__FILE__), 'restful-authentication', 'lib', "authentication", "by_password")
 require File.join(File.dirname(__FILE__), 'restful-authentication', 'lib', "authentication", "by_cookie_token")
-require File.expand_path(File.join(File.dirname(__FILE__), %w(lib fleximage)))
+require File.expand_path(File.join(File.dirname(__FILE__), %w(fleximage)))
 
 require 'action_mailer'
 
