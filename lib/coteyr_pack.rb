1a3386a267cef83b71280d754f8fe20b1301f8f5
Robert Cotey <coteyr@coteyr.net>
2012-03-07 12:55:31 -0500

    gem
    maybe

--- lib/coteyr_pack.rb
+++ lib/coteyr_pack.rb
@@ -16,3 +16,12 @@
 
 # CoteyrPack
 require File.dirname(__FILE__) + '/../lib/initilizers/exception_notifier.rb'
+require File.dirname(__FILE__) + '/../lib/semantic_form_builder.rb'
+require File.dirname(__FILE__) + '/../lib/semantic_form_helper.rb'
+require File.join('..', File.dirname(__FILE__), "..", "lib", 'restful-authentication', 'lib', "authentication")
+require File.join('..', File.dirname(__FILE__), "..", "lib", 'restful-authentication', 'lib', "authentication", "by_password")
+require File.join('..', File.dirname(__FILE__), "..", "lib", 'restful-authentication', 'lib', "authentication", "by_cookie_token")
+require File.expand_path(File.join(File.dirname(__FILE__), "..", %w(lib fleximage)))
+
+require 'action_mailer'
+
