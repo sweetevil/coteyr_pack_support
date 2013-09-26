a67da5fbf608c0322ee976328617bf13f4e857d4
Robert Cotey <coteyr@coteyr.net>
2013-09-25 20:25:38 -0400

    invert radio buttons

--- lib/coteyr_form_builder.rb
+++ lib/coteyr_form_builder.rb
@@ -76,8 +76,8 @@ class CoteyrFormBuilder < ActionView::Helpers::FormBuilder
         tag_value = 1
       end
       to_return += '<label class="radio inline">'
-      to_return += value_text
       to_return += @template.radio_button(@object_name, method, tag_value, options.merge(object: @object, class: 'style'))
+      to_return += value_text
       to_return += "</label>"
 
     end
