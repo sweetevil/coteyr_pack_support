3163e6318a7076bb18783dc883edcac0c1c5fff0
Robert Cotey <coteyr@coteyr.net>
2013-09-30 12:33:28 -0400

    faster deploy?

--- skeleton/config/deploy.rb
+++ skeleton/config/deploy.rb
@@ -66,5 +66,18 @@ namespace :log do
     invoke_command "cd #{latest_release}; RAILS_ENV=#{rails_env} bundle exec rake coteyr_pack:log:stats"
   end
 end
+
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
 #after :deploy, "passenger:restart"
 
